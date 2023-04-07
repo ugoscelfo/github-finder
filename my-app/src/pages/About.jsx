e842618c0bf06de44e1ff71322d7f5417bd979d4
Ugo Scelfo <ugoscelfo92@gmail.com>
2023-04-07 17:34:04 +0200

    added pages

---
+++ my-app/src/pages/About.jsx
@@ -0,0 +1,12 @@
+export default function About() {
+    return(
+        <div>
+            <h1 className="text-6xl mb-4">Github Finder</h1>
+            <p className="mb-4 text-2xl font-light">A react app to search Github profiles and see profile details.</p>
+            <strong>
+                <a href="https://ugoscelfo.com/about" target="_blank">Ugo Scelfo Web developer</a>
+            </strong>
+            <p className="text-lg text-gray-400">Version <span className="text-white">1.0.0</span></p>
+        </div>
+    )
+}
