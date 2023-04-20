da7bcc711aa10fe7c6bfdcbc7ab98128ea3b7cd6
Ugo Scelfo <ugoscelfo92@gmail.com>
2023-04-20 18:22:07 +0200

    alert coneìtext

--- my-app/src/components/users/UserSearch.jsx
+++ my-app/src/components/users/UserSearch.jsx
@@ -1,5 +1,7 @@
 import { useState, useContext } from "react"
 import GithubContext from "../../context/github/GithubContext"
+import AlertContext from "../../context/alert/AlertContext"
+
 
 
 export default function UserSearch() {
@@ -7,12 +9,14 @@ export default function UserSearch() {
 
     const {users, searchUsers, clearUsers} = useContext(GithubContext)
 
+    const {setAlert} = useContext(AlertContext)
+
     const handleChange = (e) => setText(e.target.value)
 
     const handleSubmit = (e) => {
         e.preventDefault()
         if(text === "") {
-            alert("Please enter something")
+            setAlert("Please enter something", "error")
         } else {
             searchUsers(text)
 
